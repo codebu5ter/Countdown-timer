01976246c77fafcd13f2b59c4a1dfe3e43cc6402
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-11-05 22:34:19 +0530

    Create Clock method

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React, {Component} from 'react';
-import './App.css'
+import React, {
+  Component
+} from 'react';
+import Clock from './Clock';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: "December 25, 2017"
+      deadline: "December 25, 2017",
+      newDeadline: ""
     }
+  }
+
+  changeDeadline() {
+    console.log("state", this.state);
+    this.setState({
+      deadline: this.state.newDeadline
+    })
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
+        <Clock deadline={this.state.deadline}/>
         <div>
-          <div className="clock-days">14 days</div>
-          <div className="clock-hours">30 hours</div>
-          <div className="clock-minutes">15 minutes</div>
-          <div className="clock-seconds">20 seconds</div>
-        </div>
-        <div>
-          <input placeholder="Enter a new date" />
-          <button>Submit</button>
+          <input placeholder="Enter a new date" onChange={event => this.setState({newDeadline: event.target.value})}/>
+          <button onClick={() => this.changeDeadline()}>
+            Submit
+          </button>
         </div>
       </div>
     )
